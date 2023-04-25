d7492a46a5f9a1a85be49717590c78fc7e3c0a78
nesrineBenyelles <121671098+nesrineBenyelles@users.noreply.github.com>
2023-04-25 13:19:42 +0100

    Add files via upload

---
+++ WelcomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function WelcomePage() {
+  return (
+    <div className='welcome-page'>
+        <h1>Hey .....! </h1>
+        <h2>Bienvenue sur LawExpertise.</h2>
+        <h3><a href='/Formulaire'>Cliquez ici</a> pour complétez votre profile </h3>
+    </div>
+  )
+}
